44d2c55d879254ef0a344dcbe8e4e5a5a01b55b7
dmnkvd <dominikvr@gmail.com>
2021-04-19 16:24:42 +0200

    remove ds store

--- dominik/00-default-main/js/dvd.js
+++ dominik/00-default-main/js/dvd.js
@@ -1,25 +1,6 @@
-// Copyright © 2008-now Harris Blondman, visual, communication, and studio. All rights reserved. www.harrisblondman.nl
-// =-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-
-
-// ! Events
-// ================================
-// $(function () {
-//   // Ready
-// });
-// $(window)
-//   .on('load', function () {
-//     // Load
-//   })
-//   .on('resize', function () {
-//     // Resize
-//   })
-//   .on('scroll', function () {
-//     // Scroll
-//   });
-
-document.addEventListener('DOMContentLoaded', (event) => {
+document.addEventListener("DOMContentLoaded", (event) => {
   //the event occurred
-})
+});
 
 // Functions
 // --------------------------------
